5aa9a0c6bf3fd05751c623b46d165129b2c90fd6
xNascimento13x <82427278+xNascimento13x@users.noreply.github.com>
2021-05-05 19:09:36 -0300

    Calculadora

---
+++ Calculadora/src/calculadora/Main.java
@@ -0,0 +1,31 @@
+package calculadora;
+
+
+import java.util.Scanner;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		
+		int n1 ;
+		 
+		int n2 ;
+		//entrada
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("qual o primeiro numero");
+		
+		n1 = input.nextInt();
+	System.out.println("qual segundo numero");
+		n2 = input.nextInt();
+		
+		System.out.println("soma: " +(n1+n2));
+		
+		
+		
+		
+
+	}
+
+}
